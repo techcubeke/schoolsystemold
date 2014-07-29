f64ba0cfec3b8f31b41e971bcc18cf753573d332
simon <smbugua11@gmail.com>
2014-07-29 16:58:39 +0300

    updated

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -4,7 +4,7 @@
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta name="language" content="en" />
-	<link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css" />
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/bootstrap.min.css" />
 	<!-- blueprint CSS framework -->
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/screen.css" media="screen, projection" />
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/print.css" media="print" />
@@ -16,6 +16,7 @@
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" />
 
 	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
+    <link href="../../../css/bootstrap.min.css" rel="stylesheet" type="text/css" />
 </head>
 
 <body>
@@ -26,7 +27,7 @@
 		<div id="logo"><?php echo CHtml::encode(Yii::app()->name); ?></div>
 	</div><!-- header -->
 
-	<div id="mainmenu">
+	<div id="mainmenu" >
 		<?php $this->widget('zii.widgets.CMenu',array(
 			'items'=>array(
 				array('label'=>'Home', 'url'=>array('/schoolinfo/')),
@@ -34,6 +35,9 @@
 				array('label'=>'Students', 'url'=>array('/student/')),
 				array('label'=>'Exams', 'url'=>array('/examrecord/')),
 				array('label'=>'Hostels', 'url'=>array('/dormroom/')),
+				array('label'=>'Vendors', 'url'=>array('/vendor/')),
+				array('label'=>'Assets', 'url'=>array('/asset/')),
+				array('label'=>'Admin', 'url'=>array('/user/')),
 				//array('label'=>'Contact', 'url'=>array('/site/contact')),
 				array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
 				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
@@ -51,7 +55,7 @@
 	<div class="clear"></div>
 
 	<div id="footer">
-		Copyright &copy; <?php echo date('Y'); ?> by My Company.<br/>
+		Copyright &copy; <?php echo date('Y'); ?> by techcube limited.<br/>
 		All Rights Reserved.<br/>
 		<?php echo Yii::powered(); ?>
 	</div><!-- footer -->
